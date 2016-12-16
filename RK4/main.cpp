c041119815596f63b3089fad021bbe41c6e81c05
Vincent Latko <vl_email@gmx.de>
2016-12-16 11:36:26 +0100

    first commit

---
+++ RK4/main.cpp
@@ -0,0 +1,24 @@
+#include <omp.h>
+#include <vector>
+
+vector<double> vVectorProduct(){
+
+}
+
+//
+vector<double> vRK4(){
+
+}
+
+//both include the external magnetic field h0
+vector<double> vCS(){
+	return ; //left side of the CS-DE for the central-spin
+}
+
+vector<double> vBS(){
+	return ; //left side of the CS-DE for the bath-spins
+}
+
+int main(){
+	return 0;
+}
