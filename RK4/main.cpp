1c0bb60ced34d07a29d7e8fede691873cb7e1f00
Vincent Latko <vl_email@gmx.de>
2016-12-21 11:41:11 +0100

    RK4

--- RK4/main.cpp
+++ RK4/main.cpp
@@ -2,101 +2,75 @@
 	ES FEHLT DAS SCHREIBEN DER AUSGABEN IN EINE .dat DATEI
 	ES FEHLEN DIE TATSÄCHLICHGEN DGLN
 	ES FEHLEN KONKRETE WERTE FÜR ZEITSCHRITTE UND FEHLER SOWIE MAXIMALE SCHRITTWEITE
-	ES FEHLT DIE IMPLEMENTIERUNG EINER WAHRSCHEINLICHKEITSVERTEILUNG FÜR DIE ANFANGSBEDINGUNGEN (GAUẞ-VERTEILUNG)
+ 	DIE INITIALISIERTEN BADSPINS PRO ZEITSCHRITT IN EINE MATRIX ALS SPALTEN EINTRAGEN
 */
 
 #include <omp.h>
-#include <vector>
+#include <Eigen/Eigenvalues>
 #include <functional>
 #include <cmath>
+#include <random>
 
+using namespace Eigen;
 using namespace std;
 
-vector<double> vectorProduct(vector<double> x, vector<double> y){
-
-	vector<double> z;
-
-	z[0] = x[1] * y[2] - x[2] * y[1];
-	z[1] = x[0] * y[2] - x[2] * y[0];
-	z[2] = x[0] * y[1] - x[1] * y[0];
+VectorXd RK4(double h, VectorXd yn, function<VectorXd(VectorXd)> f){
 	
-	return z;
-}
-
-inline vector<double> scalarVectorProd(vector<double> x, double a){
+	VectorXd yn1(3), k1(3), k2(3), k3(3), k4(3);
 	
-	for(int i = 0; i<3; i++){
-		x[i] *= a;
-	}
-
-	return x;
-}
-
-vector<double> RK4(double h, vector<double> yn, function<vector<double>(vector<double>)> f){
-	
-	vector<double> yn1, k1, k2, k3, k4;
-	
-	k1 = scalarVectorProd(f(yn), h);
-	for(int i = 0; i<3; i++){
-		yn[i] += 0.5 * k1[i];
-	}
-	k2 = scalarVectorProd(f(yn), h);
-	for(int i = 0; i<3; i++){
-		yn[i] += 0.5 * k2[i];
-	}
-	k3 = scalarVectorProd(f(yn), h);
-	for(int i = 0; i<3; i++){
-		yn[i] += k3[i];
-	}
-	k4 = scalarVectorProd(f(yn), h);
-	
-	for(int i = 0; i<3; i++){
-		yn1[i] = yn[i] + (1/6) * (k1[i] + 2*k2[i] + 2*k3[i] + k4[i]);
-	}
+	k1 = h * f(yn);
+	k2 = h * f(yn + 0.5 * k1);
+	k3 = h * f(yn + 0.5 * k2);
+	k4 = h * f(yn + k3);
+	yn1 = yn + (1/6) * (k1 + 2*k2 + 2*k3 + k4);
 
 	return yn1;
 }
 
-double stepsize(double h, double givenError, vector<double> deltaY, vector<double> yn1, function<vector<double>(vector<double>)> f){
+double stepsize(double h, double givenError, VectorXd deltaY, VectorXd yn1){
 	
-	double absDeltaY, absY = 0;
-	for(int i = 0; i<3; i++){
-		absDeltaY += deltaY[i]*deltaY[i];
-		absY += f(yn1)[i]*f(yn1)[i];
-	}
-	absDeltaY = sqrt(absDeltaY);
-	absY = sqrt(absY);
-	h *= pow(((givenError * absY)/absDeltaY), 0.2);
+	h *= pow(((givenError * yn1.norm())/deltaY.norm()), 0.2);
 
 	return h;
 }
 
-/*
-//includes the external magnetic field h0
-vector<double> BS(vector<double> si){
-	return ; //left side of the CS-DE for the bath-spins
+VectorXd init(){
+	random_device seed;
+	mt19937_64 gen(seed);
+	normal_distribution<double> distr(0., 1.);
+	VectorXd bathspin(3);
+	bathspin << distr(gen), distr(gen), distr(gen);
+	return bathspin;
 }
-*/
+
+//includes the external magnetic field h0
+VectorXd BS(VectorXd si){
+	return si; //left side of the CS-DE for the bath-spins
+}
 
 int main(){
-	/*
-	double hAdaptive = 0, givenError, hMax, h, N; //N is the number of steps the simulation has to calculate
-	vector<double> yn1;
+	//N is the number of steps the simulation has to calculate
+	double hAdaptive = 0, givenError, hMax, h, n; 
+	unsigned int nos; //nos := Number of (Bath-)Spins
+	VectorXd yn1(3), yn(3);
 
-	//actual simulation
-	for(int i = 0; i<N; i++){
-		yn1 = RK4(h, yn, BS);
-		hAdaptive = stepzise(h, givenError, RK4(2*h, yn, BS) - yn1, yn1, BS);
-
-		if(hAdaptive < h){
-			RK4(h, yn, BS);
-		}
-		if(hAdaptive > h){
-			RK4(h, yn, BS);
-			h = hAdaptive;
+	//simulation of all bathspins
+	for(int j = 0; j<nos; j++){
+		//simulation of one bathspin
+		for(int i = 0; i<n; i++){
+			//one RK4 time-step for one bathspin
+			yn1 = RK4(h, yn, BS);
+			hAdaptive = stepsize(h, givenError, RK4(2*h, yn, BS) - yn1, yn1);
+	
+			if(hAdaptive < h){
+				yn = RK4(hAdaptive, yn, BS);
+			}
+			if(hAdaptive > h){
+				yn = yn1;
+				h = hAdaptive;
+			}
 		}
 	}
 
-	*/
 	return 0;
 }
